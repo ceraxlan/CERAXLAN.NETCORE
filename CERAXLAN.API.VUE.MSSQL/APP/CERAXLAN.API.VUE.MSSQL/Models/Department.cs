9756b7ab6f8e595c5bdc59915e7afd1ef234398a
CERAXLAN <er_aslan95@outlook.com>
2022-03-21 15:23:18 +0300

    firstApplication

---
+++ CERAXLAN.API.VUE.MSSQL/APP/CERAXLAN.API.VUE.MSSQL/Models/Department.cs
@@ -0,0 +1,8 @@
+﻿namespace CERAXLAN.API.VUE.MSSQL.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
---
+++ CERAXLAN.API.VUE.MSSQL/UI/variables.js
@@ -0,0 +1,4 @@
+const variables={
+    API_URL:"http://localhost:59737/api/",
+    PHOTO_URL:"http://localhost:59737/photos/"
+}
